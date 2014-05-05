8bc64f07440d27b9e15e3ce2b79016969a63ba80
Emma Andersson <eia.andersson@gmail.com>
2014-05-05 21:11:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+include_once "inc/includes.inc.php";
+
+$path = mapRequest();
+
+routeDefault($path);
+$written = getDataFromPath($path);
+$menu = dressTemplate('menu', array('menu' => $written['menu']));
+$content ="";
+
+foreach($written['words'] as $word){
+	$content .= dressTemplate('word', $word);
+}
+
+$page = dressTemplate('page', array(
+									'title' => SITE_TITLE, 
+									'baseHref'=> SITE_ROOT, 
+									'menu' => $menu, 
+									'content' => $content, 
+									'footer' => '<small><em>Skapad av: Emma "Eia" Andersson</em></small>'));
+respond($page);
+
