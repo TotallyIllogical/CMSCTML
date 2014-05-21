9062e55b9dac0afe4387343d6639bb1a54118b9a
Emma Andersson <eia.andersson@gmail.com>
2014-05-21 21:36:41 +0200

    ...forgot jquery

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -8,7 +8,8 @@
   <title><?php print($title); ?></title>
   <base href="<?php print($baseHref); ?>">
   <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-  <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
   <div>
