7ec436581d53e8bd515d8ba085913c6b7b4b48f7
Guenhy <eia.andersson@gmail.com>
2015-02-03 16:04:49 +0100

    updated header with analytic code

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -6,6 +6,14 @@
         <meta name="description" content="My Site">
         <title><?php print($title); ?></title>
         <base href="<?php print($baseHref); ?>">
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-36488005-1', 'auto');
+            ga('send', 'pageview');
+        </script>
         <link href="style/style.css" rel="stylesheet">
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
